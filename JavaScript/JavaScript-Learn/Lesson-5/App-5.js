29fe307d17afc3441e5f766513fd2de8a5bfd2dc
aliaydn <94077022+aliaydn@users.noreply.github.com>
2022-03-01 23:18:11 +0300

    Add files via upload

---
+++ JavaScript/JavaScript-Learn/Lesson-5/App-5.js
@@ -0,0 +1,8 @@
+//var anahtar kelimedir, değişkenleri tanımlamak için kullanılır.
+//fonksiyonları tanımlamak için kullanılan anahatar kelime, "function"dur.
+//break; anahtar kelimedir. Döngüyü sonlandırmak için kullanırız.
+//continue; bir döngüden atlar ve en üstten başlar.
+//debygger; JS yürütülmesini durdurur ve hata ayıklama işlevi çağırır
+//do...while; bir koşul gerektiğinde çalıştırılır ve koşul geçersiz kalıncaya kadar tekrarlar. 
+//for; koşullar doğru olduğu sürece, yürütelecek kodlar bloğunu çalıştırır.
+//function; bir fonksiyon bildirimi yapar
