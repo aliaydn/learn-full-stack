7e9b084eadc3963ada4fc30f07b78b6337f1e6e4
aliaydn <aaydins16@gmail.com>
2022-03-06 23:43:31 +0300

    added

---
+++ JavaScript/JavaScript-Tutorials/17- İf_else Statements/App.js
@@ -0,0 +1,18 @@
+// if/else statements
+
+const firstName = 'Sena';
+const age = 19;
+const isStudent = true;
+const school = 'university';
+
+if (firstName === 'Sena') {
+    console.log('Merhaba Sena');
+}
+
+if (age === 19) {
+    console.log('yaşınız 19');
+}
+
+if (isStudent == true) {
+    console.log('Merhaba öğrenci');
+}
